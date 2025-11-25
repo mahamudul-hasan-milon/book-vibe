732da5d92163b8a41786632af76876b37ecf8910
Md. Mahamudul Hasan Milon <155943998+mahamudul-hasan-milon@users.noreply.github.com>
2025-11-25 11:42:12 +0600

    checking data

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
