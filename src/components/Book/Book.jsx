7564c2bf297ee690281379121aa6476c879d2160
Md. Mahamudul Hasan Milon <155943998+mahamudul-hasan-milon@users.noreply.github.com>
2025-11-25 12:28:26 +0600

    fetch book data and show to UI

---
+++ src/components/Book/Book.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Book = ({ book }) => {
+  const { bookId, image, bookName, author, tags, category } = book;
+  return (
+    <Link to={`/books/${bookId}`}>
+      <div className="card bg-base-100 w-96 shadow-sm p-6">
+        <figure className="bg-blue-100 py-8 rounded-2xl">
+          <img src={image} className="h-[166px]" alt={bookName} />
+        </figure>
+        <div className="card-body">
+          <div className="flex justify-center gap-4">
+            {tags.map((tag) => (
+              <button className="btn btn-xs bg-green-200 text-green-900">
+                {tag}
+              </button>
+            ))}
+          </div>
+          <h2 className="card-title">
+            {bookName}
+            <div className="badge badge-secondary">NEW</div>
+          </h2>
+          <p>By: {author}</p>
+          <div className="border-t-2 border-dashed"></div>
+          <div className="card-actions justify-between">
+            <div className="badge badge-outline">{category}</div>
+            <div className="rating">
+              <input
+                type="radio"
+                name="rating-4"
+                className="mask mask-star-2 bg-green-500"
+                aria-label="1 star"
+              />
+              <input
+                type="radio"
+                name="rating-4"
+                className="mask mask-star-2 bg-green-500"
+                aria-label="2 star"
+                defaultChecked
+              />
+              <input
+                type="radio"
+                name="rating-4"
+                className="mask mask-star-2 bg-green-500"
+                aria-label="3 star"
+              />
+              <input
+                type="radio"
+                name="rating-4"
+                className="mask mask-star-2 bg-green-100"
+                aria-label="4 star"
+              />
+              <input
+                type="radio"
+                name="rating-4"
+                className="mask mask-star-2 bg-green-100"
+                aria-label="5 star"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Book;
